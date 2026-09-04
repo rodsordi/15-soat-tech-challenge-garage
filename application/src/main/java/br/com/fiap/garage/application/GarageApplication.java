38de47daff943c81dab0e8d9bf57c6037c0feded
Rodrigo Sordi <rodsordi@gmail.com>
2026-09-04 16:16:33 -0300

    fix: register Hibernate persisters runtime hints for GraalVM native image (#19)

--- application/src/main/java/br/com/fiap/garage/application/GarageApplication.java
+++ application/src/main/java/br/com/fiap/garage/application/GarageApplication.java
@@ -1,10 +1,13 @@
 package br.com.fiap.garage.application;
 
+import br.com.fiap.commons.config.NativeRuntimeHints;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication(scanBasePackages = "br.com.fiap")
+@ImportRuntimeHints(NativeRuntimeHints.class)
 public class GarageApplication {
 
     static void main(String[] args) {
