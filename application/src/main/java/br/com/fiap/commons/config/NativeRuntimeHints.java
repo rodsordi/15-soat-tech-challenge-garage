a6831ed70aa54c2dd5ea6876445abf23f57858bc
Rodrigo Sordi <rodsordi@gmail.com>
2026-09-04 16:50:37 -0300

    perf: optimize CI/CD pipeline and register UUID reflection hints for GraalVM (#20)

--- application/src/main/java/br/com/fiap/commons/config/NativeRuntimeHints.java
+++ application/src/main/java/br/com/fiap/commons/config/NativeRuntimeHints.java
@@ -8,11 +8,28 @@ import org.hibernate.persister.entity.UnionSubclassEntityPersister;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+
+import java.util.UUID;
 
 public class NativeRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.reflection().registerType(UUID.class,
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+                MemberCategory.INVOKE_DECLARED_METHODS,
+                MemberCategory.INVOKE_PUBLIC_METHODS);
+
+        hints.reflection().registerType(UUID[].class,
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+
+        hints.reflection().registerType(TypeReference.of("java.util.UUID[]"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+
         hints.reflection().registerType(JoinedSubclassEntityPersister.class,
                 MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
                 MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
