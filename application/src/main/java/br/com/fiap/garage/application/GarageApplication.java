36fa7fcf88ee1479bd64a099d18dcfaefef8e9eb
Rodrigo <rodsordi@gmail.com>
2026-09-10 17:42:24 -0300

    fix(native): register all domain entities and enums for GraalVM AOT reflection

--- application/src/main/java/br/com/fiap/garage/application/GarageApplication.java
+++ application/src/main/java/br/com/fiap/garage/application/GarageApplication.java
@@ -1,12 +1,35 @@
 package br.com.fiap.garage.application;
 
 import br.com.fiap.commons.config.NativeRuntimeHints;
+import br.com.fiap.commons.entity.AuditableEntity;
+import br.com.fiap.commons.entity.ImmutableAuditableEntity;
+import br.com.fiap.garage.domain.entity.*;
+import br.com.fiap.garage.domain.enums.MaterialType;
+import br.com.fiap.garage.domain.enums.WorkOrderStatus;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication(scanBasePackages = "br.com.fiap")
+@RegisterReflectionForBinding({
+        Customer.class,
+        Employee.class,
+        Vehicle.class,
+        Email.class,
+        Notification.class,
+        Material.class,
+        InventoryMaterial.class,
+        Service.class,
+        EstimatedService.class,
+        EstimatedMaterial.class,
+        WorkOrder.class,
+        AuditableEntity.class,
+        ImmutableAuditableEntity.class,
+        MaterialType.class,
+        WorkOrderStatus.class
+})
 @ImportRuntimeHints(NativeRuntimeHints.class)
 public class GarageApplication {
 
