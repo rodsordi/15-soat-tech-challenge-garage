9ba5bfb44eeb1b09d4e68c0dad38472e22e62b3b
Rodrigo <rodsordi@gmail.com>
2026-09-10 17:54:18 -0300

    fix(jpa): define PersistenceManagedTypes bean for GraalVM entity scanning

--- application/src/main/java/br/com/fiap/commons/config/JpaConfig.java
+++ application/src/main/java/br/com/fiap/commons/config/JpaConfig.java
@@ -5,10 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypes;
+import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypesScanner;
+
 @Configuration
 @EnableJpaAuditing
 @EntityScan("br.com.fiap")
 @EnableJpaRepositories("br.com.fiap")
 public class JpaConfig {
 
+    @Bean
+    public PersistenceManagedTypes persistenceManagedTypes(ResourceLoader resourceLoader) {
+        return new PersistenceManagedTypesScanner(resourceLoader)
+                .scan("br.com.fiap.garage.domain.entity", "br.com.fiap.commons.entity");
+    }
 }
